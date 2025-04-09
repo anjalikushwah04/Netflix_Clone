a9f7d018fc6786d9b30ae8ff241b28d50d1f7c73
anjalikushwah04 <kushwahanjali600@gmail.com>
2025-04-09 20:38:37 +0530

    initial commit

---
+++ frontend/src/pages/home/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import AuthScreen from './AuthScreen';
+import HomeScreen from './HomeScreen';
+import { useAuthStore } from '../../store/authUser';
+
+const HomePage = () => {
+  const {user}= useAuthStore();
+  return (
+    <>
+      {user ? <HomeScreen/>:<AuthScreen/>}
+    </>
+  )
+}
+
+export default HomePage
